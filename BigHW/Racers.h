a23ea5310c0038c0d0ab72d348149e04157da1dc
Zsolt <fzsolt9001@gmail.com>
2020-05-25 12:23:21 +0200

    Update Racers.h
    Added hello Lőrinc

--- BigHW/Racers.h
+++ BigHW/Racers.h
@@ -5,11 +5,13 @@ typedef struct racecar {
 	double top_speed; 
 	double acceleration;
 	double cornering speed;
-	double[9] DNA;
+	double[18] DNA;
 }racecar;
 
 typedef struct driver {
-
+	double cooperativeness;
+	double Hello_lõrinc;
+	
 };
 
 typedef class Racer
