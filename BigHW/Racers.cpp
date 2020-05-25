5af3287a98f790017499a470c3ba3ed7cdadef8b
Zsolt <fzsolt9001@gmail.com>
2020-05-25 16:54:57 +0200

    started

--- BigHW/Racers.cpp
+++ BigHW/Racers.cpp
@@ -1,1 +1,2 @@
 #include "Racers.h"
+
--- BigHW/Racers.h
+++ BigHW/Racers.h
@@ -2,19 +2,28 @@
 #include <vector>
 
 typedef struct racecar {
-	double top_speed; 
-	double acceleration;
-	double cornering_speed;
-	double DNA[18];
+	double top_speed; //70
+	double acceleration; //10
+	double cornering_speed; //30
+	int DNA[18];
 }racecar;
 
 typedef struct driver {
 	double cooperativeness;
-
-};
+	int DNA[32];
+}driver;
 
 typedef class Racer
 {
+	driver driver_object;
+	racecar car_object;
+	double position_on_truck;
+	vector<double> lap_times; // self measured time
+	int status; //0: acceleration, 1: top speed, 2: braking 3: at cornering speed
+	std::vector<std::pair<double, int>>& track;
 
+	//Racer();
+	Racer(driver& d, racecar& rc);
+	void reset();
 }racer;
 
