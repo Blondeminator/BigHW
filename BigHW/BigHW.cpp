787093892ec845d6eae1a3c4cf530857be01a4a6
IamTheRealBambi <65897089+IamTheRealBambi@users.noreply.github.com>
2020-05-25 15:13:34 +0200

    Na végre?

--- BigHW/BigHW.cpp
+++ BigHW/BigHW.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
 #include "Racers.h"
+#include <vector>
+
+using namespace std;
+
+vector<pair<double, int>> truck0;
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    truck0.resize();
+    for (int i = 0; i < 500; i++) {
+        truck0[i].first = i;
+        truck0[i].second = 0;
+    }
+    for (int i = 501; i < 1500; i++) {
+        truck0[i].first = i;
+        truck0[i].second = 0;
+    }
+    for (int i = 1501; i < 2500; i++) {
+        truck0[i].first = i;
+        truck0[i].second = 0;
+    }
+    for (int i = 2501; i < 3500; i++) {
+        truck0[i].first = i;
+        truck0[i].second = 0;
+    }
+    for (int i = 3501; i < 4000; i++) {
+        truck0[i].first = i;
+        truck0[i].second = 0;
+    }
+    truck0[500].first = -1;
+    truck0[1500].first = -1;
+    truck0[2500].first = -1;
+    truck0[3500].first = -1;
+   
 }
 
